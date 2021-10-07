99a1b8815833da3ddf79ae06da9cd9a5961420f7
antonWetzel <anton.wetzel.mail@gmail.com>
2021-10-07 14:33:32 +0200

    test, calculate center is ts

--- out/main.js
+++ out/main.js
@@ -8,7 +8,28 @@ import * as Cloud from './gpu/cloud.js';
 import * as KNearest from './gpu/kNearest.js';
 import { CreateColors } from './loader/color.js';
 import { CreateGrid } from './loader/grid.js';
+import { Center } from './test.js';
 document.body.onload = async () => {
+    const points = new Float32Array([
+        /*eslint-disable*/
+        Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, 0,
+        Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, 0,
+        Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, 0,
+        0, 0, 0, 0,
+        /*eslint-enable*/
+    ]);
+    const pointsColors = new Float32Array([
+        /*eslint-disable*/
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        1, 1, 1, 0,
+        /*eslint-enable*/
+    ]);
+    const c = Center({ x: points[0], y: points[1], z: points[2] }, { x: points[4], y: points[5], z: points[6] }, { x: points[8], y: points[9], z: points[10] });
+    points[12] = c.x;
+    points[13] = c.y;
+    points[14] = c.z;
     const display = document.getElementById('display');
     const canvas = await GPU.Setup(display.clientWidth, display.clientHeight);
     if (canvas == undefined) {
@@ -33,10 +54,13 @@ document.body.onload = async () => {
     const increase = new Position();
     increase.Scale(5, 5, 5);
     const normal = new Position();
-    let length = 100_000;
+    //let length = 100_000
+    let length = 4;
+    let cloud = GPU.CreateBuffer(points, GPUBufferUsage.VERTEX | GPUBufferUsage.STORAGE);
+    let colors = GPU.CreateBuffer(pointsColors, GPUBufferUsage.VERTEX | GPUBufferUsage.STORAGE);
     let form = 'sphere';
-    let cloud = CreateSphere(length);
-    let colors = CreateColors(length);
+    //let cloud = CreateSphere(length)
+    //let colors = CreateColors(length)
     const grid = CreateGrid(10);
     display.onwheel = (ev) => {
         const scale = 1 + ev.deltaY / 1000;
@@ -153,7 +177,7 @@ document.body.onload = async () => {
             cam.Translate(0, dist, 0);
         }
         GPU.StartRender(cam);
-        await Cloud.Render(increase, 0.02, length, cloud, colors);
+        await Cloud.Render(increase, 1, length, cloud, colors);
         await Lines.Render(normal, grid.length, grid.positions, grid.colors);
         if (nearest != undefined) {
             await KNearest.Render(increase, cloud, colors, nearest.buffer, nearest.k, length);
