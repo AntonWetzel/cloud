11328b6154995189c19ccd8c6eeda5c4aedb7e6b
antonWetzel <anton.wetzel.mail@gmail.com>
2021-11-04 15:02:33 +0100

    fix pages

--- dist/main.c39d6dcf.js
+++ dist/main.c39d6dcf.js
@@ -2159,7 +2159,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62060" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59721" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
